4c293cd5efac26719664bd01ce9accc4c2fa2c7f
LouisCatala <86700310+LouisCatala@users.noreply.github.com>
2021-11-07 18:54:31 -0500

    Update math.js

--- generators/python/math.js
+++ generators/python/math.js
@@ -211,7 +211,7 @@ Blockly.Python['math_number_property'] = function(block) {
       var divisor = Blockly.Python.valueToCode(block, 'DIVISOR',
           Blockly.Python.ORDER_MULTIPLICATIVE);
       // If 'divisor' is some code that evals to 0, Python will raise an error.
-      if (!divisor || divisor == '0') {
+      if (!divisor || divisor === '0') {
         return ['False', Blockly.Python.ORDER_ATOMIC];
       }
       code = number_to_check + ' % ' + divisor + ' == 0';
